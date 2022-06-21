b958015f2b06c0f469b7cd0e5570a361c408b54f
Shannon Pileggi <pileggis@mskcc.org>
2022-06-21 19:58:21 -0400

    add white space

--- 01_explore-libraries_solution.R
+++ 01_explore-libraries_solution.R
@@ -50,9 +50,11 @@ ipt %>%
 
 ## is every package in .Library either base or recommended?
 all_default_pkgs <- list.files(.Library)
+
 all_br_pkgs <- ipt %>%
   filter(Priority %in% c("base", "recommended")) %>%
   pull(Package)
+
 setdiff(all_default_pkgs, all_br_pkgs)
 
 ## study package naming style (all lower case, contains '.', etc
@@ -60,6 +62,7 @@ setdiff(all_default_pkgs, all_br_pkgs)
 ## use `fields` argument to installed.packages() to get more info and use it!
 ipt2 <- installed.packages(fields = "URL") %>%
   as_tibble()
+
 ipt2 %>%
   mutate(github = grepl("github", URL)) %>%
   count(github) %>%
